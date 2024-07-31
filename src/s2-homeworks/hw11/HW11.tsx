b376ad138cb2710620cdf6621457749150760e2e
Nik <tabola1000@gamil.com>
2024-07-31 21:16:54 +0300

    remake end position not start

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -4,37 +4,23 @@ import s2 from '../../s1-main/App.module.css';
 import { restoreState } from '../hw06/localStorage/localStorage';
 import SuperRange from './common/c7-SuperRange/SuperRange';
 
-/*
-* 1 - передать значения в оба слайдера
-* 2 - дописать типы и логику функции change
-* 3 - сделать стили в соответствии с дизайном
-* */
-
 function HW11() {
-    // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0));
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100));
 
     const change = (event: Event, value: number | number[]) => {
         if (Array.isArray(value)) {
             const [newValue1, newValue2] = value;
-            if (newValue1 < value2) {
-                setValue1(newValue1);
-            }
-            if (newValue2 > value1) {
-                setValue2(newValue2);
-            }
+            setValue1(newValue1);
+            setValue2(newValue2);
         } else {
-            if (value < value2) {
-                setValue1(value);
-            }
+            setValue1(value);
         }
     };
 
     return (
         <div id={'hw11'} className={s2.mainBlock}>
             <div className={s2.hwTitle}>Homework #11</div>
-
             <div className={s2.hw}>
                 <div className={s.container}>
                     <div className={s.wrapper}>
@@ -45,10 +31,8 @@ function HW11() {
                             onChange={change}
                             disableSwap
                             size="small"
-                            // сделать так чтоб value1 изменялось // пишет студент
                         />
                     </div>
-
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
@@ -57,7 +41,6 @@ function HW11() {
                             disableSwap
                             value={[value1, value2]}
                             onChange={change}
-                            // сделать так чтоб value1/2 изменялось // пишет студент
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,24 +1,8 @@
-import React from 'react'
-import {Slider as SliderN, SliderProps} from '@mui/material'
+import React from 'react';
+import { Slider as SliderN, SliderProps } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
-
-
-
-const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
-}
-
-export default SuperRange
-
-
-const Slider = styled(SliderN)(({theme}) => ({
+const Slider = styled(SliderN)(({ theme }) => ({
     color: '#00CC22',
     width: '150px',
     height: 4,
@@ -42,4 +26,16 @@ const Slider = styled(SliderN)(({theme}) => ({
         color: theme.palette.mode === 'dark' ? '#bfbfbf' : '#8B8B8B',
         opacity: theme.palette.mode === 'dark' ? undefined : 1,
     },
-}))
+}));
+
+const SuperRange: React.FC<SliderProps> = (props) => {
+    return (
+        <Slider
+            sx={{ // стили для слайдера
+            }}
+            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+        />
+    )
+}
+
+export default SuperRange;
